91d6fca585f963b9e1a2155f38ddac531da5b78f
sudipbhanja123 <sudipbhanja5g@gmail.com>
2024-07-14 14:23:00 +0530

    minr change

--- src/components/Randomquotes.js
+++ src/components/Randomquotes.js
@@ -4,33 +4,34 @@ const RandomQuotes = () => {
   const [quote, setQuote] = useState("");
   const [error, setError] = useState(null);
 
-  const fetchQuote = async (retries = 3) => {
-    try {
-      const response = await fetch(
-        "https://api.api-ninjas.com/v1/quotes?category=inspirational",
-        {
-          headers: {
-            "X-Api-Key": "uoVE1+qdLGjWc3oDpxpMGQ==9Z99ESxUqwgdEybd",
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.statusText}`);
-      }
-      const data = await response.json();
-      setQuote(data[0].quote);
-      setError(null);
-    } catch (error) {
-      console.error("Fetching quote failed:", error.message);
-      if (retries > 0) {
-        setTimeout(() => fetchQuote(retries - 1), 1000);
-      } else {
-        setError("Fetching quote failed");
-      }
-    }
-  };
-
   useEffect(() => {
+    const fetchQuote = async (retries = 3) => {
+      try {
+        const response = await fetch(
+          "https://api.api-ninjas.com/v1/quotes?category=inspirational",
+          {
+            headers: {
+              "X-Api-Key": "uoVE1+qdLGjWc3oDpxpMGQ==9Z99ESxUqwgdEybd",
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error(
+            `Network response was not ok: ${response.statusText}`
+          );
+        }
+        const data = await response.json();
+        setQuote(data[0].quote);
+        setError(null);
+      } catch (error) {
+        console.error("Fetching quote failed:", error.message);
+        if (retries > 0) {
+          setTimeout(() => fetchQuote(retries - 1), 1000);
+        } else {
+          setError("Fetching quote failed");
+        }
+      }
+    };
     fetchQuote();
     const interval = setInterval(() => fetchQuote(), 5000);
     return () => clearInterval(interval);
