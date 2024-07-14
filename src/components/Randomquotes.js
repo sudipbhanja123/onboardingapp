e5f312516963a7d8e3346ea6070ecad138181e04
sudipbhanja123 <sudipbhanja5g@gmail.com>
2024-07-14 14:18:59 +0530

    api fetch done

--- src/components/Randomquotes.js
+++ src/components/Randomquotes.js
@@ -1,56 +1,45 @@
-// import React, { useEffect, useState } from "react";
-
-// const RandomQuotes = () => {
-//   const [quote, setQuote] = useState("");
-
-//   useEffect(() => {
-//     const fetchQuote = async () => {
-//       const response = await fetch("https://api.quotable.io/random");
-//       const data = await response.json();
-//       setQuote(data.content);
-//     };
-//     fetchQuote();
-//     const interval = setInterval(fetchQuote, 5000);
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return (
-//     <div className="bg-gray-200 p-4 rounded-lg mt-4">
-//       <p className="text-center text-sm italic">{quote}</p>
-//     </div>
-//   );
-// };
-
-// export default RandomQuotes;
-
 import React, { useEffect, useState } from "react";
 
 const RandomQuotes = () => {
   const [quote, setQuote] = useState("");
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchQuote = async () => {
-      try {
-        const response = await fetch("https://api-ninjas.com/api/quotes");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+  const fetchQuote = async (retries = 3) => {
+    try {
+      const response = await fetch(
+        "https://api.api-ninjas.com/v1/quotes?category=inspirational",
+        {
+          headers: {
+            "X-Api-Key": "uoVE1+qdLGjWc3oDpxpMGQ==9Z99ESxUqwgdEybd",
+          },
         }
-        const data = await response.json();
-        setQuote(data.content);
-      } catch (error) {
-        setError("Network response was not ok");
+      );
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
       }
-    };
+      const data = await response.json();
+      setQuote(data[0].quote);
+      setError(null);
+    } catch (error) {
+      console.error("Fetching quote failed:", error.message);
+      if (retries > 0) {
+        setTimeout(() => fetchQuote(retries - 1), 1000);
+      } else {
+        setError("Fetching quote failed");
+      }
+    }
+  };
+
+  useEffect(() => {
     fetchQuote();
-    const interval = setInterval(fetchQuote, 5000);
+    const interval = setInterval(() => fetchQuote(), 5000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="bg-gray-200 p-4 rounded-lg mt-4">
       {error ? (
-        <p className="text-center text-sm">{error}</p>
+        <p className="text-center text-sm ">{error}</p>
       ) : (
         <p className="text-center text-sm italic">{quote}</p>
       )}
